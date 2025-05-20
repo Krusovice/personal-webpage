930ceb5b4a7e7abe36bd0235a607df8766bd4157
Krusovice <jmk1589@gmail.com>
2025-05-21 00:11:35 +0200

    moved django bash command to django docker folder and changed django docker folder to hosting folder

---
+++ django/hosting/django_bash_command.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DJANGO_ENV=production_no
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+chown -R www-data:www-data /django/staticfiles
+chmod -R 755 /django/staticfiles
+
+python manage.py shell -c "from literature.models import Item; from django.core.management import call_command; call_command('loaddata', 'literature/fixtures/literature.json') if not Item.objects.exists() else None"
+
+if [ \"$DJANGO_ENV\" = \"production\" ]; then
+  daphne -b 0.0.0.0 -p 8000 personalWebpage.asgi:application
+else
+  pip uninstall -y daphne
+  python manage.py runserver 0.0.0.0:8000
+fi
