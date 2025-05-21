743423d67f151eeb3f27216ef2f5481d2e60a80b
Krusovice <jmk1589@gmail.com>
2025-05-21 16:26:55 +0200

    about section

--- django/about/urls.py
+++ django/about/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.about_index, name='about_index'),
 ]
--- django/about/views.py
+++ django/about/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'about/home.html')
+def about_index(request):
+    return render(request, 'about/about_index.html')
