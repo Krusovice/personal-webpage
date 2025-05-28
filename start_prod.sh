58228b4e1616d3fdbad898f7a73f45a122c5bf4f
Krusovice <jmk1589@gmail.com>
2025-05-28 23:56:52 +0200

    setting user rights in start_prod.sh

--- start_prod.sh
+++ start_prod.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-set -e  # Exit on first error
+set -e
 
 NETWORK_NAME="webpage_network"
 
-echo "🔍 Checking for Docker network: $NETWORK_NAME..."
+echo "🔧 Fixing folder permissions..."
 
+# Ensure correct ownership and access
+sudo chown -R "$USER:$USER" ./postgres/postgres_data || true
+chmod -R 755 ./postgres/postgres_data || true
+
+sudo chown -R "$USER:$USER" ./redis/redis_data || true
+chmod -R 755 ./redis/redis_data || true
+
+chmod +x django/hosting/django_bash_command.sh
+
+echo "✅ Permissions fixed."
+
+echo "🔍 Checking for Docker network: $NETWORK_NAME..."
 if ! docker network ls --format '{{.Name}}' | grep -q "^${NETWORK_NAME}$"; then
     echo "🛠  Network '$NETWORK_NAME' not found. Creating it..."
     docker network create "$NETWORK_NAME"
