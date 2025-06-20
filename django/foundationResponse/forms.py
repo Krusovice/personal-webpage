51d18215f4b33d54ae95e0670f1b15c8557c5466
Krusovice <jmk1589@gmail.com>
2025-06-21 00:15:16 +0200

    test form implemented

--- django/foundationResponse/forms.py
+++ django/foundationResponse/forms.py
@@ -1,6 +1,24 @@
 from django import forms
 
 class Foundation_response_linear_regression(forms.Form):
-    width = forms.FloatField(label="Width")
-    eccentricity = forms.FloatField(label="Eccentricity")
-    soils = forms.FloatField(label="Soil E-modulus")
+    width = forms.FloatField(
+        label="Width",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter width"
+        })
+    )
+    eccentricity = forms.FloatField(
+        label="Eccentricity",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter eccentricity"
+        })
+    )
+    soils = forms.FloatField(
+        label="Soil E-modulus",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter soil E-modulus"
+        })
+    )
