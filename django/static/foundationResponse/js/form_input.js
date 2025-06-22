a37f560843f680f66b4004e06eab4115777afa50
Krusovice <jmk1589@gmail.com>
2025-06-23 00:11:27 +0200

    foundation-response-predictor form

--- django/static/foundationResponse/js/form_input.js
+++ django/static/foundationResponse/js/form_input.js
@@ -2,28 +2,43 @@ document.addEventListener('DOMContentLoaded', () => {
   const tbody = document.getElementById('dynamic-table');
 
   // 1) Add new row when last one is filled
-  tbody.addEventListener('input', e => {
-    const rows = tbody.querySelectorAll('.input-row');
-    const last = rows[rows.length - 1];
-    if (!last.contains(e.target)) return;
+    tbody.addEventListener('input', e => {
+        const rows = tbody.querySelectorAll('.input-row');
+        const rowsCount = rows.length;
+        const lastRow = rows[rowsCount - 1];
+        if (!lastRow.contains(e.target)) return; // If its not the last row that is the trigger.
 
-    const inputs = Array.from(last.querySelectorAll('input'));
-    if (!inputs.every(i => i.value.trim() !== '')) return;
+        const inputs = Array.from(lastRow.querySelectorAll('input'));
+        if (inputs.some(i => i.value.trim() == '')) return;
 
-    const clone = last.cloneNode(true);
-    clone.querySelectorAll('input').forEach(i => i.value = '');
-    tbody.appendChild(clone);
-  });
+        const clone = lastRow.cloneNode(true);
+        clone.querySelectorAll('input').forEach(i => i.value = '');
+        tbody.appendChild(clone);
+    });
 
-  // 2) Delete row when delete button is clicked
-  tbody.addEventListener('click', e => {
-    if (e.target && e.target.classList.contains('delete-row')) {
-      const row = e.target.closest('tr');
+  // 2) Delete row
+    tbody.addEventListener('click', e => {
+        if (e.target && e.target.classList.contains('delete-row')) {
+            const row = e.target.closest('tr');
 
-      // Prevent deleting the last remaining row (optional)
-      if (tbody.querySelectorAll('tr').length > 1) {
-        row.remove();
-      }
-    }
-  });
+          // Prevent deleting the last remaining row (optional)
+            if (tbody.querySelectorAll('tr').length > 1) {
+                row.remove();
+            }
+        }
+    });
+
+  // 3) Add row
+    tbody.addEventListener('click', e => {
+        if (e.target && e.target.classList.contains('insert-row')) {
+            const row = e.target.closest('tr');
+
+            const newRow = row.cloneNode(true);
+            newRow.querySelectorAll('input').forEach(i => i.value = '');
+            tbody.insertBefore(newRow, row);
+        }
+    });
+
+    // Fading delete button if only one child
+    tbody.addEventListener()
 });
