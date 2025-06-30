4816fefc82d7cbd1bb54bf36c80d399a11a5be24
Krusovice <jmk1589@gmail.com>
2025-06-30 09:51:04 +0200

    added navbar empty links

--- django/foundationResponse/urls.py
+++ django/foundationResponse/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='foundationResponse_index'),
+    path('models/', views.home, name='foundationResponse_index'),
     path('linear_regression/', views.linear_regression, name='linear_regression')
 ]
