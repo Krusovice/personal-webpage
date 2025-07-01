c006bc707e755414546aafe17e37b13fb854c8e3
Krusovice <jmk1589@gmail.com>
2025-07-01 20:57:07 +0200

    navbar icon

--- django/static/js/navbar.js
+++ django/static/js/navbar.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.getElementById("navbar-side");
+  const sidebarIcon = document.getElementById("navbar-side-icon");
   sidebar.style.transition = "none";
 
   const navbarLinks = document.querySelectorAll(".navbar-link");
@@ -16,18 +17,32 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-
-
-  // activate sidebar
+  // activate sidebar by hover
   sidebar.addEventListener("mouseover", () => {
     document.documentElement.classList.add("sidebar-open");
     document.cookie = "sidebarOpen=true; path=/; Secure";
   });
 
-  // deavtivating sidebar
+  // deavtivating sidebar by hover
   sidebar.addEventListener("mouseout", () => {
-    document.documentElement.classList.remove("sidebar-open");
-    document.cookie = "sidebarOpen=false; path=/; Secure";
+    if (document.cookie.includes("sidebarOpenFixed=false")) {
+      document.documentElement.classList.remove("sidebar-open");
+      document.cookie = "sidebarOpen=false; path=/; Secure";
+    }
   });
+
+  // activate sideby by icon
+  sidebarIcon.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("sidebar-open")) {
+      document.documentElement.classList.remove("sidebar-open");
+      document.cookie = "sidebarOpen=false; path=/; Secure";
+      document.cookie = "sidebarOpenFixed=false; path=/; Secure";
+    } else {
+      document.documentElement.classList.add("sidebar-open");
+      document.cookie = "sidebarOpen=true; path=/; Secure";
+      document.cookie = "sidebarOpenFixed=true; path=/; Secure";
+    }
+  });
+
 
 });
