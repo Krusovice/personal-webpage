15a473bf74b8f0472e7b3b1230b36e79d90665ef
Krusovice <jmk1589@gmail.com>
2025-10-20 22:45:57 +0200

    started frontend on react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+// App.tsx
+import Frontpage from "./pages/Frontpage";
+
+export default function App() {
+  return (
+    <Frontpage />
+  );
+}
---
+++ frontend/src/pages/Frontpage.tsx
@@ -0,0 +1,8 @@
+
+// Frontpage.tsx
+
+export default function Frontpage() {
+  return (
+    <h1>I'm a front page</h1>
+  )
+}
