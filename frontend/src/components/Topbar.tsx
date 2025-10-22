253e0b1706198db7e00e48f94f0030563b9f89b6
Krusovice <jmk1589@gmail.com>
2025-10-22 22:52:54 +0200

    added topbar items

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -4,7 +4,30 @@ import stylesTopbar from "./../styles/TopbarStyling.module.css"
 export default function Topbar() {
   return (
     <div className={ stylesTopbar.topbar }>
-      <h1>Topbar</h1>  
+
+      <ul className={ stylesTopbar.list }>
+        <li>
+          <a className= {stylesTopbar.link} href="/">News</a>
+        </li>
+
+        <li>
+          <a className= {stylesTopbar.link} href="/">Literature DB</a>
+        </li>
+
+        <li>
+          <a className= {stylesTopbar.link} href="/">Selected Stocks</a>
+        </li>
+
+        <li>
+          <a className= {stylesTopbar.link} href="/">Foundation Response</a>
+        </li>
+
+        <li className={stylesTopbar.right}>
+          <a className= {stylesTopbar.link} href="/">Profile</a>
+        </li>
+
+      </ul>
+
     </div> 
   )
 }
