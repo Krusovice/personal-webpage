af248d469379900468b4c9d1a18e73bf9cf1c59e
Krusovice <jmk1589@gmail.com>
2025-10-22 22:21:30 +0200

    added fading edges

--- frontend/src/components/Edges.tsx
+++ frontend/src/components/Edges.tsx
@@ -5,10 +5,10 @@ export default function Edges() {
   return (
     <>
       <div className={stylesEdges.left_edge}>
-        <h1>Left edge</h1>
+        <h1></h1>
       </div>
       <div className={stylesEdges.right_edge}>
-        <h1>Right edge</h1>
+        <h1></h1>
       </div>
     </>
   )
