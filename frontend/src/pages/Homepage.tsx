0740361f2f401cd886fbc7a2fc176fff8df7c070
Krusovice <jmk1589@gmail.com>
2025-10-28 21:11:39 +0100

    rebuilding announcement

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -4,7 +4,7 @@ import stylesMainArea from "./../styles/MainAreaStyling.module.css"
 export default function Homepage() {
   return (
     <div className={ stylesMainArea.mainArea }>
-      <h1>I'm the home page</h1>  
+      <h1>The site is rebuilt from djagno to React+Rust backend.</h1>  
     </div>
     
   )
