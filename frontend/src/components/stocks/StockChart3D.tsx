33396abb4f1caa2616d1b29d5333a496623c3fce
Krusovice <jmk1589@gmail.com>
2025-12-03 08:56:24 +0100

    normalizing graph values and plotting x-axis

--- frontend/src/components/stocks/StockChart3D.tsx
+++ frontend/src/components/stocks/StockChart3D.tsx
@@ -10,60 +10,109 @@ type SimplePricePoint = {
 
 // Mock data
 const mockData: SimplePricePoint[] = [
-  { date: "Day 1", close: 100 },
-  { date: "Day 2", close: 105 },
-  { date: "Day 3", close: 102 },
-  { date: "Day 4", close: 110 },
-  { date: "Day 5", close: 108 },
+  { date: "2025-12-03", close: 100 },
+  { date: "2025-12-04", close: 105 },
+  { date: "2025-12-07", close: 102 },
+  { date: "2025-12-08", close: 110 },
+  { date: "2025-12-12", close: 108 },
 ];
 
 // Functions
+// Graph plot values are normalized between 0 and 1.
 
-// Data array to be plotted
+// Normalized values to be plotted
 function buildPointsFromData(data) {
   const points = [];
+  const xValues = [];
+  const zValues = [];
 
-  data.forEach((point, index) => {
-    const x = index;
+  if (!data.length) {
+    return points;
+  }
+
+  // collect raw x (time) and z (close) values
+  data.forEach((point) => {
+    xValues.push(new Date(point.date).getTime());
+    zValues.push(point.close);
+  });
+
+  const xMin = Math.min(...xValues);
+  const xMax = Math.max(...xValues);
+  const zMin = Math.min(...zValues);
+  const zMax = Math.max(...zValues);
+
+  const xRange = xMax - xMin || 1;
+  const zRange = zMax - zMin || 1;
+
+  data.forEach((point) => {
+    const xValue = new Date(point.date).getTime();
+    const xNorm = (xValue - xMin) / xRange;
     const y = 0;
-    const z = point.close;
+    const zValue = point.close;
+    const zNorm = (zValue - zMin) / zRange;
 
-    points.push([x, y, z]);
+    points.push([xNorm, y, zNorm]);
   });
 
   return points;
 }
 
-// Center coordinate for graph
-function centerOrbit(data) {
-  const x = [];
-  const z = [];
-
-  data.forEach((point, index) => {
-    x.push(index);
-    z.push(point.close);
-  });
-
-  const xMin = Math.min(...x);
-  const xMax = Math.max(...x);
-  const zMin = Math.min(...z);
-  const zMax = Math.max(...z);
-
-  const xCenter = (xMin + xMax) / 2;
-  const zCenter = (zMin + zMax) / 2;
-
-  return [xCenter, 0, zCenter];
-}
 
 // Z-axis, containing values
 function zAxis(data, numberOfTicks) {
-  const tickValues = [];
   const tickValuesObject = [];
-  const z = [];
+  const zValues = [];
 
   data.forEach((point) => {
-    z.push(point.close);
+    zValues.push(point.close);
   });
+
+  const zMin = Math.min(...zValues);
+  const zMax = Math.max(...zValues);
+  const zInc = (zMax-zMin)/numberOfTicks;
+  
+  // Normalizing graph loc values, so theyre always between 0 and 1.
+  // For proper text sizing.
+  const zMin_loc = 0;
+  const zMax_loc = 1;
+  const zInc_loc = (zMax_loc-zMin_loc)/numberOfTicks;
+
+  // Line
+  const linePoint_0 = [0,0,zMin_loc];
+  const linePoint_1 = [0,0,zMax_loc];
+  const line = <Line points={[linePoint_0,linePoint_1]} dashed={false} />;
+
+  // Labels
+  for (let i = 0; i < numberOfTicks; i++) {
+    let tickValue_loc = zMin_loc + i*zInc_loc;
+    let tickValue = zMin + i*zInc;
+
+    tickValuesObject.push(
+      <Text position={[0,0,tickValue_loc]} fontSize={0.05} rotation={[-Math.PI / 2, 0, 0]}>
+        {tickValue}
+      </Text>
+    );
+  };
+
+  return [line, ...tickValuesObject];
+}
+
+// X-axis, containing dates
+function xAxis(data, numberOfTicks) {
+  const tickValues = [];
+  const tickValuesObject = [];
+  const x = [];
+  const xText = [];
+
+  data.forEach((point) => {
+    x.push( Date(point.date).getTime() );
+    xText.push( point.date );
+
+
+
+  });
+
+
 
   const zMin = Math.min(...z);
   const zMax = Math.max(...z);
@@ -81,7 +130,7 @@ function zAxis(data, numberOfTicks) {
   }
   tickValues.forEach((value) => {
     tickValuesObject.push(
-      <Text position={[0,0,value]} fontSize={0.4} rotation={[-Math.PI / 2, 0, 0]}>
+      <Text position={[0,0,value]} fontSize={0.1} rotation={[-Math.PI / 2, 0, 0]}>
         {value}
       </Text>
     );
@@ -92,30 +141,21 @@ function zAxis(data, numberOfTicks) {
     tickValuesObject]
   )
 
+
 }
-
-// X-axis, containing dates
-
 
 
 
 export default function StockChart3D() {
   // Convert mockData into 3D points for the Line
   const points = buildPointsFromData(mockData);
-  const orbitCoord = centerOrbit(mockData);
-  const cameraPosition = [...orbitCoord];
-  cameraPosition[1] += 10;
-
 
   return (
     <div style={{ width: "100%", height: "400px" }}>
-      <Canvas camera={{ position: cameraPosition, fov: 50 }}>
-        {/* some basic light so we can see things */}
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+      <Canvas camera={{ position: [0.5,2,0.5], fov: 50 }}>
 
         {/* mouse control: rotate, pan, zoom */}
-        <OrbitControls target={orbitCoord}/>
+        <OrbitControls target={[0.5,0.5,0.5]}/>
 
         {zAxis(mockData, 10)}
 
