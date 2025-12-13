87b91932c7d022b56a37d0aafa5f5597bef166d8
Krusovice <jmk1589@gmail.com>
2025-12-13 22:46:45 +0100

    stocks, sorting ticker data by date from backend

--- backend/src/handlers/stocks/fetch_stocks.rs
+++ backend/src/handlers/stocks/fetch_stocks.rs
@@ -33,6 +33,7 @@ pub async fn get_stock_data(
         FROM stockmarket.stock_prices
         WHERE ticker = $1
         AND date >= $2
+        SORT BY date
         "#
     )
     .bind(&request.ticker)
