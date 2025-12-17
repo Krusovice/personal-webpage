fffc1cdfc5d45d279854e72cfae2aa3de250ccc3
Krusovice <jmk1589@gmail.com>
2025-12-17 15:45:04 +0100

    stocks stockSelected flexbox item positions issue, where items expanded the container

---
+++ frontend/src/components/foundationResponse/FRContent.tsx
@@ -0,0 +1,12 @@
+import styling from "../../styles/foundationResponse/FRStyling.module.css"
+
+export default function FRContent() {
+
+
+
+  return (
+    <div className={styling.FRArea}>
+      flaps
+    </div>
+  )
+}
--- frontend/src/pages/Foundationpage.tsx
+++ frontend/src/pages/Foundationpage.tsx
@@ -1,11 +1,10 @@
-
 import styles from "./../styles/Content.module.css"
+import FRContent from "./../components/foundationResponse/FRContent"
 
 export default function Foundationpage() {
   return (
     <div className={ styles.content }>
-      <h1>I'm the foundation page</h1>  
+      <FRContent/>
     </div>
-    
   )
 }
