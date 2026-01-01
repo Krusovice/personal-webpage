12576e88040197c43192d4ebc27a79318361f04c
Krusovice <jmk1589@gmail.com>
2026-01-01 22:59:05 +0100

    literature app, bug with content width fixed

--- frontend/src/pages/Literaturepage.tsx
+++ frontend/src/pages/Literaturepage.tsx
@@ -1,13 +1,10 @@
 import styles from "./../styles/Content.module.css"
-import stylesLiteraturePage from "./../styles/LiteratureStyling.module.css"
-//import LiteratureSearch from "./../components/LiteratureSearch"
 import LiteratureContent from "./../components/literature/Literature"
 
 
 export default function Literaturepage() {
   return (
-    <div className={[styles.content, stylesLiteraturePage.literaturePage].join(' ')}>
-      {/* <LiteratureSearch /> */}
+    <div className={ styles.content }>
       <LiteratureContent/>
     </div>
     
