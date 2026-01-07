5ba50e403717e4b77114e68f9ea939627c849240
Krusovice <jmk1589@gmail.com>
2026-01-07 08:14:14 +0100

    started server monitor component

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,14 @@
-import CalculatorInput from "./Calculator.tsx"
-import ServerMonitorGraph from "./ServerMonitor.tsx"
+import CalculatorInput from "./Calculator.tsx";
+import ServerMonitor from "./serverMonitor/ServerMonitor.tsx";
 
 
-import stylesSidebar from "./../../styles/sidebar/SidebarStyling.module.css"
+import stylesSidebar from "./../../styles/sidebar/SidebarStyling.module.css";
 
 export default function Sidebar() {
   return (
     <div className={ stylesSidebar.sidebar }>
       <CalculatorInput/>
-      <ServerMonitorGraph/>
+      <ServerMonitor/>
     </div>
   )
 }
---
+++ frontend/src/components/sidebar/serverMonitor/ServerMonitor.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import ServerMonitorGraph from "./ServerMonitorGraph.tsx"
+
+import layoutStyling from "./../../../styles/LayoutStyling.module.css"
+import styling from "./../../../styles/sidebar/ServerMonitor.module.css"
+
+
+export default function ServerMonitor() {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className={`${layoutStyling.subWindowDark} ${styling.monitorGraph}` }>
+      
+      <ServerMonitorGraph/>
+
+    </div>
+  );
+}
