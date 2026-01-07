6b6d6700ade702eff6fc552d60250d9bc508cc27
Krusovice <jmk1589@gmail.com>
2026-01-07 16:54:15 +0100

    monitor graph

--- frontend/src/components/sidebar/serverMonitor/ServerMonitorGraph.tsx
+++ frontend/src/components/sidebar/serverMonitor/ServerMonitorGraph.tsx
@@ -1,21 +1,23 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Line, Text } from "@react-three/drei";
+import * as THREE from "three";
 
 export type Point3 = [number, number, number];
 
 export default function ServerMonitorGraph() {
+  const center = new THREE.Vector3(0.5, 0, 0.5);
 
   function xAxis() {
-    const linePoint_0: Point3 = [-1,0,0];
+    const linePoint_0: Point3 = [0,0,0];
     const linePoint_1: Point3 = [1,0,0];
     const line = <Line key="xAxis" points={[linePoint_0,linePoint_1]} dashed={false} />;
     return line
   }
 
-  function yAxis() {
+  function zAxis() {
     const linePoint_0: Point3 = [0,0,0];
-    const linePoint_1: Point3 = [-1,0,1];
-    const line = <Line key="yAxis" points={[linePoint_0,linePoint_1]} dashed={false} />;
+    const linePoint_1: Point3 = [0,0,1];
+    const line = <Line key="zAxis" points={[linePoint_0,linePoint_1]} dashed={false} />;
     return line
   }
 
@@ -23,14 +25,21 @@ export default function ServerMonitorGraph() {
 
   return(
     <Canvas 
+      orthographic
       camera={{
-        position: [0.5,-1,0],
-        fov: 10,
-        up: [0, 0, 1]
+        position: [0.5,-5,0.5],
+        zoom: 200,
+        near: 0.1,
+        far: 1000,
+        up: [0, 0, -1],
+      }}
+      onCreated={({ camera }) => {
+        camera.lookAt(center);
+        camera.updateProjectionMatrix();
       }}
     >
       {xAxis()}
-      {yAxis()}
+      {zAxis()}
     </Canvas>
     )
 }
