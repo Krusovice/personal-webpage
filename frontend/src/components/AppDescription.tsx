d36d83161eb69a588e949d9093c8a51fc8d70365
Krusovice <jmk1589@gmail.com>
2026-01-09 22:53:39 +0100

    general app layout

--- frontend/src/components/AppDescription.tsx
+++ frontend/src/components/AppDescription.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import layoutStyling from "./../styles/LayoutStyling.module.css"
+//import { useState } from "react";
+//import layoutStyling from "./../styles/LayoutStyling.module.css"
 import styling from "./../styles/literature/LiteratureStyling.module.css"
 export default function AppDescription() {
-  const [docsOpen, setDocsOpen] = useState(false);
-  const [descOpen, setDescOpen] = useState(false);
+  //const [docsOpen, setDocsOpen] = useState(false);
+  //const [descOpen, setDescOpen] = useState(false);
   
   return(
     <div className={`${styling.appInfoArea}`}>
